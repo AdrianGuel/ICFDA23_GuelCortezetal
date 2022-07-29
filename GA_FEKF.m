07e6d3864665f423f85413a235aecfa343b7860b
A.-J. Guel-Cortez <52336176+AdrianGuel@users.noreply.github.com>
2022-07-29 17:42:06 +0100

    Update GA_FEKF.m

--- GA_FEKF.m
+++ GA_FEKF.m
@@ -1,28 +1,31 @@
 %Guel-Cortez et al 2022. 
 %Parameter Estimation of Fractional-Order Systems via Evolutionary Algorithms and the Extended Fractional Kalman Filter
+%General script for fractional EKF with GA in a mass spring damper system
 
 close all;
 clearvars;
 clc
 
 timerVal=tic;
+rng('default');
+rng(1); %1 error 1.0893
 
-L = 10;
-Tspan=50;
-T=0.05;
+L = 100;
+Tspan=25;
+T=.05;
 t=0:T:Tspan;
-ks=2;
-b=0.5;
+ks=1.5;
+b=0.8;
 N=[.8;.8];
-Q=1e-5*eye(2,2);
-R=1e-3;
-u=sin(t);
+R=1e-4;
+Q=1e-6*eye(2,2);
+u=5*sin(t);
 [t,y]=FOsystem(ks,b,N,Q,R,T,t,L,u);
-Q=1e-6*eye(5,5);
+Q=1e-6*eye(5,5); Q(3,3)=0; Q(4,4)=0;Q(5,5)=0;
 realp=[ks,b,N(1)];
 
 options = optimoptions('ga','PlotFcn',"gaplotbestf",'UseParallel', true,'MaxStallGenerations',500,'MaxGenerations',1000);
-[x,fval] = ga(@(gains) FOKFilter(t,u,y,gains,Q,R,T,L),3,[],[],[],[],[0;0;0],[3;1;1],[],options);
+[x,fval] = ga(@(gains) FOKFilter(t,u,y,gains,Q,R,T,L),3,[],[],[],[],[-1;-1;0],[2;2;1],[],options);
 
 [RMSE,te,ye,x_e]=FOKFilter(t,u,y,x,Q,R,T,L);
 figure 
@@ -57,6 +60,6 @@ plot(t,x_e(5,:))
 xlabel('t (s)','Interpreter','Latex','FontSize', 12)
 ylabel('$\alpha_e(t)$','Interpreter','Latex','FontSize', 12)
 
-estimatedp=[x_e(3:5,end)];
+estimatedp=[x_e(3:5,end)]
 norm(realp-estimatedp,2)
 Elapsetime=toc(timerVal);
